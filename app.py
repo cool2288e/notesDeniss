9d8e87125890888ee55729232cb566e47ed77edd
cool2288e <mobistyle02062022@gmail.com>
2024-11-30 15:25:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -22,10 +22,61 @@ class NoteWindow(QtWidgets.QMainWindow):
 
         self.load_notes()
 
- def load_notes(self):
-    try:
-        with open("notes_data.json","r",encoding="ulf-8") as file:
-            self.notes = json.load(file)
-        self.ui.listWidget.addItems(self.notes)
-    except FileNotFoundError:
-        self.notes = {}
+    def load_notes(self):
+        try:
+            with open("notes_data.json","r",encoding="ulf-8") as file:
+                self.notes = json.load(file)
+            self.ui.listWidget.addItems(self.notes)
+        except FileNotFoundError:
+            self.notes = {}
+            self.notes = {}
+
+    def save_to_file(self):
+        with open("notes_data.json", "w", encoding="utf-8") as file:
+            json.dump(self.notes, file, sort_keys=True,
+                      ensure_ascii=False)
+
+    def add_note(self):
+            note_name, ok = QtWidgets.QInputDialog.getText(
+                self, "Додати замітку", "Назва замітки: " )
+            if ok and note_name != "":
+                self.notes[note_name] = {"текст": "", "теги": []}
+                self.ui.listWidget.addItem(note_name)
+                self.save_to_file()
+    def show_note(self):
+        key = self.ui.listWidget.currentItem().text()
+        self.ui.textEdit.setText(self.notes[key]["текст"])
+        self.ui.listWidget_2.clear()
+        self.ui.listWidget_2.addItems(self.notes[key]["теги"])
+
+    def save_note(self):
+        if self.ui.listWidget.currentItem():
+            key = self.ui.listWidget.currentItem().text()
+            self.notes[key]["текст"] = self.ui.textEdit.toPlainText()
+            self.save_to_file()
+        else:
+            QtWidgets.QMessageBox.warning(self, "Помилка", "Замітка для збереження не вибрана!")
+
+    def del_note(self):
+        if self.ui.listWidget.currentItem():
+            key = self.ui.listWidget.currentItem().text()
+            del self.notes[key]
+            self.ui.listWidget.clear()
+            self.ui.listWidget_2.clear()
+            self.ui.textEdit.clear()
+            self.ui.listWidget.addItems(self.notes)
+            self.save_to_file()
+        else:
+            QtWidgets.QMessageBox.warning(self, "Помилка", "Замітка для видалення не обрана!")
+
+    def add_tag(self):
+        if self.ui.listWidget.currentItem():
+            key = self.ui.listWidget.currentItem().text()
+            tag = self.ui.lineEdit.text()
+            if tag and tag not in self.notes[key]["теги"]:
+                self.notes[key]["теги"].append(tag)
+                self.ui.listWidget_2.addItem(tag)
+                self.ui.lineEdit.clear()
+                self.save_to_file()
+        else:
+            QtWidgets.QMessageBox.warning(self, "Помилка", "Замітка для додавання тега не обрана!")
